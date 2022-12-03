6facc7e17ea2b307cb654a9c6f6acdbf7294f794
linqirong <qirong77@linqirongdeMacBook-Air.local>
2022-12-03 23:48:31 +0800

    chore: 整理代码,准备用vue重写,优化逻辑,简化功能

--- src/main/helper/getWindow.ts
+++
@@ -1,7 +0,0 @@
-import { BrowserWindow } from 'electron'
-
-export const getWindow = (e: Electron.IpcMainEvent | Electron.IpcMainInvokeEvent) => {
-  return BrowserWindow.getAllWindows().find((window) => {
-    return window.id === e.sender.id
-  })
-}
--- src/main/helper/moveFile.ts
+++ src/main/helper/moveFile.ts
@@ -2,8 +2,6 @@ import { lstatSync, readdirSync, renameSync } from 'fs'
 
 export const removePath = (path: string, newDir: string) => {
   if (readdirSync(newDir).includes(path)) throw new Error('文件已经存在！')
-  console.log(path)
-  console.log(newDir)
   if (lstatSync(path).isDirectory()) {
     renameSync(path,newDir)
   } else {
